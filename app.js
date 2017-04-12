c6ad3db19c698e013dcacd70cc88c29865ca07b2
Sebastian Gabor <s.gabor@itgambit.com>
2017-04-12 14:29:30 +0200

    commit for heroku

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/hello', function() {
   res.send("world");
 
@@ -10,3 +15,11 @@ app.get('/hello', function() {
 app.post('/webhook', function (req, res) {
   res.send('hello world');
 })
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
